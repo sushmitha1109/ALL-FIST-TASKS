543caadfc544391890227f966677e082b782d43b
SUSHMITHA.SK <51911682+sushmitha1109@users.noreply.github.com>
2019-09-10 12:28:38 +0530

    Completed task

--- Task 1(practice)/work.js
+++ Task 1(practice)/work.js
@@ -1,10 +1,13 @@
+var currentRow;
+var rows;
+var newrow;
 $(document).ready(function()
 {
- 
-  var a = 0;
+    
+
          $("#add").click(function()
           { 
-         var rows = " ";
+         
          var firstname = document.getElementById("inputName4").value;
          var lastname= document.getElementById("inputLName4").value;
          var email= document.getElementById("inputEmail").value;
@@ -25,6 +28,7 @@ $(document).ready(function()
            rows +=' <td>'+state+'</td>';
           
            rows +='<td><button class="edit-btn">'+"edit"+"</button></td>";
+           rows +='<td><button class="save-btn">'+"save"+"</button></td>";
            rows +='<td><button class="remove-btn">'+"remove"+"</button></td></tr>";
         
            
@@ -38,7 +42,7 @@ $(document).ready(function()
                document.getElementById("inputCity").value=" ";
                document.getElementById("inputState").value=" ";
               
-
+               rows=" ";
                
            });
           
@@ -49,7 +53,7 @@ $(document).ready(function()
           
            $("#mytable").on('click', '.edit-btn', function () {
           
-              var currentRow=$(this).closest("tr"); 
+               currentRow=$(this).closest("tr"); 
               
               var col1 = currentRow.find("td:eq(0)").text(); 
               var col2 = currentRow.find("td:eq(1)").text(); 
@@ -68,12 +72,52 @@ $(document).ready(function()
               $("#inputAddress2").val(col6);
               $("#inputCity").val(col7);
               $("#inputstate").val(col8);
-              
-
+            
             });
-              
+            
+              $("#mytable").on('click', '.save-btn', function () {
+ 
+             
+             var firstname = document.getElementById("inputName4").value;
+             var lastname= document.getElementById("inputLName4").value;
+             var email= document.getElementById("inputEmail").value;
+             var password= document.getElementById("inputPassword").value;
+             var Address1 = document.getElementById("inputAddress1").value;
+             var Address2= document.getElementById("inputAddress2").value;
+             var city = document.getElementById("inputCity").value;
+             var state = document.getElementById("inputState").value;
+        
+      
+             newrow +=' <tr><td>'+firstname+'</td>';
+             newrow +=' <td>'+lastname+'</td>';
+             newrow +=' <td>'+email+'</td>';
+             newrow +=' <td>'+password+'</td>';
+             newrow +=' <td>'+Address1+'</td>';
+             newrow +=' <td>'+Address2+'</td>';
+             newrow +=' <td>'+city+'</td>';
+             newrow +=' <td>'+state+'</td>';
 
-         });
+             newrow +='<td><button class="edit-btn">'+"edit"+"</button></td>";
+             newrow +='<td><button class="save-btn">'+"save"+"</button></td>";
+             newrow +='<td><button class="remove-btn">'+"remove"+"</button></td></tr>";
+               
+         
+           $(currentRow).replaceWith(newrow);
+            currentRow=" ";
+            newrow=" ";
+             document.getElementById("inputName4").value=" " ;
+             document.getElementById("inputLName4").value=" ";
+             document.getElementById("inputEmail").value=" ";
+             document.getElementById("inputPassword").value=" ";
+             document.getElementById("inputAddress1").value=" ";
+             document.getElementById("inputAddress2").value=" ";
+             document.getElementById("inputCity").value=" ";
+             document.getElementById("inputState").value=" ";
+           });
+              
+       });       
+
+  
 
        
             
